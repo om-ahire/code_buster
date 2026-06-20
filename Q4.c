ea031309f6e47a74aac2cefbf8144454ff00fd02
OM AHIRE <omahire2601@gmail.com>
2026-06-20 11:57:36 +0530

    Update Q4.c

--- Q4.c
+++ Q4.c
@@ -4,12 +4,17 @@ int main()
     char str[100];
     int l = len(str);
     for (int i = 0; i < l; i++)
+        int count = 4;
+
     {
         printf("Enter the string");
         scanf("%s", &str);
     }
     for (int i = 0; i < l; i++)
     {
-        if (str[i] =)
+        if (len(str) > 4)
+        {
+            printf(str);
+        }
     }
 }
